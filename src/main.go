b2050618593dae9aa32e17739de4e7eeb4cb5f76
Alan Lu <a@28int.com>
2014-10-25 22:09:41 +0800

    Complete parsing.

--- src/main.go
+++ src/main.go
@@ -6,7 +6,10 @@ import (
 	"net/http"
 	"io/ioutil"
 	"os"
-	//"code.google.com/p/go.net/html"
+	"strings"
+	"time"
+	//"runtime"
+	"code.google.com/p/go.net/html"
 )
 
 func get(date string) string {
@@ -49,22 +52,161 @@ func testGet() string {
 }
 
 type Record struct {
-	Date uint
-	Price float32
+	Date time.Time
+	Buy float32
+	Sell float32
 }
 
-func parse(content string) []Record {
+type Parser struct {
+	Content string
+}
+
+func NewParser(content string) *Parser {
 	if len(content) == 0 {
 		return nil
 	}
 
-	slice := make([]Record, 0)
+	return &Parser {
+		Content: content,
+	}
+}
 
+func (p *Parser) parse() []Record {
+	doc, err := html.Parse(strings.NewReader(p.Content))
+	if err != nil {
+		return nil
+	}
+
+	slice := make([]Record, 0)
+	var date time.Time
+    var f func(*html.Node)
+    f = func(n *html.Node) {
+        if n.Type == html.ElementNode {
+			// Date
+			if n.Data == "th" && n.FirstChild != nil{
+				if n.FirstChild.Data == "資料日期" {
+					content := n.NextSibling.NextSibling.FirstChild.Data
+					date = p.getDate(content)
+				}
+			}
+
+			// Records
+			if n.Data == "tr" {
+				for _, attr := range n.Attr {
+					if attr.Key == "class" {
+						if attr.Val == "color0" || attr.Val == "color1" {
+						record := p.getRecord(n, date)
+						slice = append(slice, record)
+						}
+					}
+				}
+			}
+		}
+
+        for node := n.FirstChild; node != nil; node = node.NextSibling {
+            f(node)
+        }
+    }
+    f(doc)
 
 	return slice
 }
 
+func (p *Parser) getDate(content string) time.Time {
+	if len(content) == 0 {
+		return time.Date(0, time.Month(0), 0, 0, 0, 0, 0, time.UTC)
+	}
+
+	var year, month, day int
+	_, err := fmt.Sscanf(content, "%d/%d/%d", &year, &month, &day)
+	if err != nil {
+		log.Printf("Parser.getDate has error, sscanf error with %s\n", content)
+		return time.Date(0, time.Month(0), 0, 0, 0, 0, 0, time.UTC)
+	}
+
+	location, err := time.LoadLocation("Asia/Taipei")
+	if err != nil {
+		log.Printf("Parser.getDate has error, %s\n", err)
+		return time.Date(0, time.Month(0), 0, 0, 0, 0, 0, time.UTC)
+	}
+
+	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, location)
+}
+
+func (p *Parser) getRecord(node *html.Node, date time.Time) Record {
+	// Time
+	newNode := node.FirstChild
+	if newNode != nil {
+		var hour, minute int
+		str := newNode.FirstChild.Data
+		_, err := fmt.Sscanf(str, "%02d:%02d", &hour, &minute)
+		if err == nil {
+			duration := time.Duration(hour) * time.Hour
+			duration += time.Duration(minute) * time.Minute
+			date = date.Add(duration)
+		}
+	}
+
+	// Buy price
+	newNode = newNode.NextSibling.NextSibling.NextSibling
+	var buy float32
+	if newNode != nil {
+		if newNode.FirstChild.Data != "" {
+			str := newNode.FirstChild.Data
+			var price float32
+			_, err := fmt.Sscanf(str, "%f", &price)
+			if err == nil {
+				buy = price
+			}
+		}
+	}
+
+	// Sell price
+	newNode = newNode.NextSibling
+	var sell float32
+	if newNode != nil {
+		if newNode.FirstChild.Data != "" {
+			str := newNode.FirstChild.Data
+			var price float32
+			_, err := fmt.Sscanf(str, "%f", &price)
+			if err == nil {
+				sell = price
+			}
+		}
+	}
+	return Record {
+		Date: date,
+		Buy: buy,
+		Sell: sell,
+	}
+}
+
 func main() {
 	str := testGet()
-	log.Println(str)
+	parser := NewParser(str)
+	for _, record := range parser.parse() {
+		date := record.Date
+		str := fmt.Sprintf("%04d/%02d/%02d %02d:%02d", date.Year(), date.Month(), date.Day, date.Hour(), date.Minute())
+		log.Printf("%s, buy: %.0f, sell: %.0f\n", str, record.Buy, record.Sell)
+	}
+
+	/*
+	NCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(NCPU)
+	const MAX = 100000
+	ch := make(chan string, NCPU)
+	for i := 0; i < MAX; i++ {
+		go func() {
+			ch <- testGet()
+		}()
+	}
+
+	slice := make([]string, 0)
+	for i := 0; i < MAX; i++ {
+		if str := <-ch; str != "" {
+			parse(str)
+		}
+	}
+	log.Printf("cap(slice): %d\n", cap(slice))
+	*/
 }
